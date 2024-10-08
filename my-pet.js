87d571b164193542a5ca0fbe1fce488c3b4f94f7
Liska Crofts <38409100+LiskaC@users.noreply.github.com>
2024-10-08 12:27:25 +0200

    Adds files for students to practice initialising variables

---
+++ my-pet.js
@@ -0,0 +1,9 @@
+// My pets
+
+const species = 'dog'
+const name = 'Ptolemy II Philobabushka'
+const age = 6
+const numberOfPaws = 4
+const hasTail = true
+const previousOwner = null
+const romanticInterest = undefined
---
+++ your-pet.js
@@ -0,0 +1,4 @@
+// Create your pet in variables!
+
+// Here's variable to get you started.
+const species = '?'
