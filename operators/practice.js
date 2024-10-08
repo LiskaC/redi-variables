955665f3f103b4f2f07563eb96bfa47f40a1e459
Liska Crofts <38409100+LiskaC@users.noreply.github.com>
2024-10-08 13:54:27 +0200

    Add practice exercises

---
+++ operators/practice.js
@@ -0,0 +1,55 @@
+const TwinSistersAges = 26
+const BrothersAge = 17
+
+// example: Log the total age of the sisters:
+
+const totalAgeOfTwinSisters = TwinSistersAges * 2
+
+console.log(totalAgeOfTwinSisters)
+// OR:
+console.log('the total age of the twin sisters is: ' + totalAgeOfTwinSisters)
+// (would actually usually do this:):
+console.log(`the total age of the twin sisters is: ${totalAgeOfTwinSisters}`)
+
+// ________________________________________________________________________
+
+// 1. Log the age difference between the sisters and the brother:
+
+
+// ________________________________________________________________________
+const ageToGetIntoClub = 18
+
+// 2. Log whether the sisters can get into the club:
+
+
+
+// ________________________________________________________________________
+
+// 3. Log the total age of all the siblings, with the text 'The total age of all the siblings is': 
+
+
+
+// ________________________________________________________________________
+
+// 4. Log whether the brother and sisters can all get into the club together:
+
+
+
+// ________________________________________________________________________
+
+// 5. Log whether the total age of the sisters, divided by the total number of siblings, is greater
+//    than the brother's age:
+
+
+
+// ________________________________________________________________________
+
+// 6. Log whether the brother's age, multiplied by the total number of siblings, is 'smaller than
+//    or equal to' the sisters' combined age:
+
+
+// ________________________________________________________________________
+
+// 7. Log a sister saying her age in the sentence 'Hurrah, I am xxxxx years old, I can go in da Club!'
+
+
